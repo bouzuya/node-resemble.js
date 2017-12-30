4347bbae7bc378e175cfeb77cc72e87dc517a433
bouzuya <m@bouzuya.net>
2017-12-30 15:10:08 +0900

    extract parse-compare-images-options

---
+++ src/compare-images-options.ts
@@ -0,0 +1,71 @@
+import { newTransformer } from './error-pixel-transformer';
+import { newTolerance } from './tolerance';
+import { U8 } from './type/u8';
+import { CompareImagesOptions } from './type/compare-images-options';
+import { IgnoreType } from './type/ignore-type';
+import { Pixel } from './type/pixel';
+import { Rectangle } from './type/rectangle';
+import { ResembleOptions } from './type/resemble-options';
+import { Tolerance } from './type/tolerance';
+
+const parseCompareImagesOptions = (
+  options?: ResembleOptions
+): CompareImagesOptions => {
+  const opts = options || {};
+  const getColorValue = (
+    color: ResembleOptions['errorColor'],
+    key: 'red' | 'green' | 'blue' | 'alpha',
+    defaultValue: U8
+  ): U8 => {
+    if (typeof color === 'undefined') return defaultValue;
+    const value = color[key];
+    if (typeof value === 'undefined') return defaultValue;
+    return value;
+  };
+  const errorPixelColor: Pixel = {
+    r: getColorValue(opts.errorColor, 'red', 255),
+    g: getColorValue(opts.errorColor, 'green', 0),
+    b: getColorValue(opts.errorColor, 'blue', 255),
+    a: getColorValue(opts.errorColor, 'alpha', 255)
+  };
+  const errorPixelTransformer = newTransformer(
+    errorPixelColor,
+    typeof opts.errorType !== 'undefined' ? opts.errorType : 'flat'
+  );
+  const transparency = typeof opts.transparency !== 'undefined'
+    ? opts.transparency
+    : 1;
+  const largeImageThreshold = typeof opts.largeImageThreshold !== 'undefined'
+    ? opts.largeImageThreshold
+    : 1200;
+  const ignoreType: IgnoreType =
+    typeof opts.ignoreAlpha !== 'undefined' && opts.ignoreAlpha
+      ? 'alpha'
+      : typeof opts.ignoreAntialiasing !== 'undefined' && opts.ignoreAntialiasing
+        ? 'antialiasing'
+        : typeof opts.ignoreColors !== 'undefined' && opts.ignoreColors
+          ? 'colors'
+          : typeof opts.ignoreLess !== 'undefined' && opts.ignoreLess
+            ? 'less'
+            : typeof opts.ignoreNothing !== 'undefined' && opts.ignoreNothing
+              ? 'nothing'
+              : 'default';
+  const tolerance: Tolerance = newTolerance(ignoreType);
+  const ignoreAntialiasing = ignoreType === 'antialiasing';
+  const ignoreColors = ignoreType === 'colors';
+  const ignoreRectangles: Rectangle[] | null =
+    typeof opts.ignoreRectangles !== 'undefined'
+      ? opts.ignoreRectangles
+      : null;
+  return {
+    largeImageThreshold,
+    ignoreAntialiasing,
+    ignoreColors,
+    ignoreRectangles,
+    transparency,
+    tolerance,
+    errorPixelTransformer
+  };
+};
+
+export { parseCompareImagesOptions };
--- src/compare-images.ts
+++ src/compare-images.ts
@@ -1,11 +1,9 @@
 import { getHue, getLightness } from './color';
-import { newTransformer } from './error-pixel-transformer';
-import { newTolerance } from './tolerance';
+import { parseCompareImagesOptions } from './compare-images-options';
 import { U8 } from './type/u8';
 import { CompareImagesOptions } from './type/compare-images-options';
 import { CompareResult } from './type/compare-result';
 import { FileNameOrData } from './type/file-name-or-data';
-import { IgnoreType } from './type/ignore-type';
 import { Image } from './type/image';
 import { Pixel } from './type/pixel';
 import { Rectangle } from './type/rectangle';
@@ -223,70 +221,16 @@ const analyseImages = (
   };
 };
 
-const parseOptions = (options?: ResembleOptions): CompareImagesOptions => {
-  const opts = options || {};
-  const getColorValue = (
-    color: ResembleOptions['errorColor'],
-    key: 'red' | 'green' | 'blue' | 'alpha',
-    defaultValue: U8
-  ): U8 => {
-    if (typeof color === 'undefined') return defaultValue;
-    const value = color[key];
-    if (typeof value === 'undefined') return defaultValue;
-    return value;
-  };
-  const errorPixelColor: Pixel = {
-    r: getColorValue(opts.errorColor, 'red', 255),
-    g: getColorValue(opts.errorColor, 'green', 0),
-    b: getColorValue(opts.errorColor, 'blue', 255),
-    a: getColorValue(opts.errorColor, 'alpha', 255)
-  };
-  const errorPixelTransformer = newTransformer(
-    errorPixelColor,
-    typeof opts.errorType !== 'undefined' ? opts.errorType : 'flat'
-  );
-  const transparency = typeof opts.transparency !== 'undefined'
-    ? opts.transparency
-    : 1;
-  const largeImageThreshold = typeof opts.largeImageThreshold !== 'undefined'
-    ? opts.largeImageThreshold
-    : 1200;
-  const ignoreType: IgnoreType =
-    typeof opts.ignoreAlpha !== 'undefined' && opts.ignoreAlpha
-      ? 'alpha'
-      : typeof opts.ignoreAntialiasing !== 'undefined' && opts.ignoreAntialiasing
-        ? 'antialiasing'
-        : typeof opts.ignoreColors !== 'undefined' && opts.ignoreColors
-          ? 'colors'
-          : typeof opts.ignoreLess !== 'undefined' && opts.ignoreLess
-            ? 'less'
-            : typeof opts.ignoreNothing !== 'undefined' && opts.ignoreNothing
-              ? 'nothing'
-              : 'default';
-  const tolerance: Tolerance = newTolerance(ignoreType);
-  const ignoreAntialiasing = ignoreType === 'antialiasing';
-  const ignoreColors = ignoreType === 'colors';
-  const ignoreRectangles: Rectangle[] | null =
-    typeof opts.ignoreRectangles !== 'undefined'
-      ? opts.ignoreRectangles
-      : null;
-  return {
-    largeImageThreshold,
-    ignoreAntialiasing,
-    ignoreColors,
-    ignoreRectangles,
-    transparency,
-    tolerance,
-    errorPixelTransformer
-  };
-};
-
-const compareImages = (file1: FileNameOrData, file2: FileNameOrData, options?: ResembleOptions): Promise<CompareResult> => {
+const compareImages = (
+  file1: FileNameOrData,
+  file2: FileNameOrData,
+  options?: ResembleOptions
+): Promise<CompareResult> => {
   return Promise
     .all([loadImage(file1), loadImage(file2)])
     .then(([image1, image2]: [Image, Image]) => {
       //lksv: normalization removed
-      return analyseImages(image1, image2, parseOptions(options));
+      return analyseImages(image1, image2, parseCompareImagesOptions(options));
     });
 };
 
