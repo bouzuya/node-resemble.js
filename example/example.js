9e31dd25907e4da8f9d574b00a08a2db7d520ede
bouzuya <m@bouzuya.net>
2017-12-31 00:06:12 +0900

    update example.js

--- example/example.js
+++ example/example.js
@@ -1,7 +1,7 @@
-var resemble = require('../resemble.js');
-var fs = require('fs');
+const { compareImages, loadImage } = require('../.tmp/src'); // @bouzuya/resemble
+const fs = require('fs');
 
-resemble.outputSettings({
+const options = {
   errorColor: {
     red: 155,
     green: 100,
@@ -9,41 +9,42 @@ resemble.outputSettings({
   },
   errorType: 'movement',
   transparency: 0.6
-});
+};
 
-resemble('People.png').compareTo('People2.png')
-  //.ignoreAntialiasing()
-  //.ignoreColors()
-  .onComplete(function(data){
+Promise
+  .all([loadImage('People.png'), loadImage('People2.png')])
+  .then(([image1, image2]) => {
+    const data = compareImages(image1, image2, options);
     console.log(data);
     data.getDiffImage().pack().pipe(fs.createWriteStream('diff.png'));
   });
 
 // jpeg comparison
-resemble('People.jpg').compareTo('People2.jpg')
-  //.ignoreAntialiasing()
-  //.ignoreColors()
-  .onComplete(function(data){
+Promise
+  .all([loadImage('People.jpg'), loadImage('People2.jpg')])
+  .then(([image1, image2]) => {
+    const data = compareImages(image1, image2, options);
     console.log(data);
     data.getDiffImage().pack().pipe(fs.createWriteStream('diffjpg.png'));
   });
 
 // jpeg comparison
-resemble('People.jpg').compareTo('People2.jpg')
-  //.ignoreAntialiasing()
-  //.ignoreColors()
-  .onComplete(function(data){
+Promise
+  .all([loadImage('People.jpg'), loadImage('People2.jpg')])
+  .then(([image1, image2]) => {
+    const data = compareImages(image1, image2, options);
     console.log(data);
     fs.writeFileSync('diffjpg.jpg', data.getDiffImageAsJPEG());
   });
 
-var fileData1 = fs.readFileSync('People.png');
-var fileData2 = fs.readFileSync('People2.png');
-resemble(fileData1).compareTo(fileData2)
-  //.ignoreAntialiasing()
-  //.ignoreColors()
-  .ignoreRectangles([[325,170,100,40]])
-  .onComplete(function(data){
+const fileData1 = fs.readFileSync('People.png');
+const fileData2 = fs.readFileSync('People2.png');
+Promise
+  .all([loadImage(fileData1), loadImage(fileData2)])
+  .then(([image1, image2]) => {
+    const data = compareImages(image1, image2, Object.assign({}, options, {
+      ignoreRectangles: [[325, 170, 100, 40]]
+    }));
     console.log('with ignore rectangle:', data);
     data.getDiffImage().pack().pipe(fs.createWriteStream('diffr.png'));
   });
